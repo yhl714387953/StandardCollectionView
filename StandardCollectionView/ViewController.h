03ce05e5c45879723c1a820526b411e88999a022
hailong <714387953@qq.com>
2015-06-06 15:44:37 +0800

    Initial Commit

---
+++ StandardCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StandardCollectionView
+//
+//  Created by Hailong Yu on 15/6/6.
+//  Copyright (c) 2015年 zhongkeyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StandardCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StandardCollectionView
+//
+//  Created by Hailong Yu on 15/6/6.
+//  Copyright (c) 2015年 zhongkeyun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
